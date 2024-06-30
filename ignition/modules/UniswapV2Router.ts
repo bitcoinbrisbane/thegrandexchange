cb34774e07d0c42f1b0b1f5ef22db3853c7ebf7b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-30 14:19:53 +1000

    chore: Update UniswapV2Router to use WETH as a parameter

--- ignition/modules/UniswapV2Router.ts
+++ ignition/modules/UniswapV2Router.ts
@@ -2,7 +2,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const UniswapV2RouterModule = buildModule("UniswapV2RouterModule", (m) => {
   const fee_setter = "0x7988123D1F90ccF9675f9D154870Af0f9274DF91";
-  const factory = m.contract("UniswapV2Factory", [fee_setter]);
+  const weth = "0x8C271dF2DF93110e690feaB3C428B2708F664955";
+  const factory = m.contract("UniswapV2Router01", [fee_setter, weth]);
 
   return { factory };
 });
