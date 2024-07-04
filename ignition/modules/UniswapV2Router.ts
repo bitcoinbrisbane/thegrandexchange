bd3f37fecf2b33bb588b6d7642333f5e3cfcc32c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-07-04 14:02:07 +1000

    chore: Add debug artifacts for MockUSDTModule, MockwBTCModule, and UniswapV2FactoryModule

--- ignition/modules/UniswapV2Router.ts
+++ ignition/modules/UniswapV2Router.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const UniswapV2RouterModule = buildModule("UniswapV2RouterModule", (m) => {
   const fee_setter = "0x7988123D1F90ccF9675f9D154870Af0f9274DF91";
-  const weth = "0x8C271dF2DF93110e690feaB3C428B2708F664955";
+  const weth = "0xc665C290BaCA0709d66327320206d7c65e2A6F36";
   const factory = m.contract("UniswapV2Router01", [fee_setter, weth]);
 
   return { factory };
---
+++ scripts/router.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const ethers = require("ethers");
+const { JsonRpcProvider } = require("@ethersproject/providers");
+
+require("dotenv").config();
+
+const contractABI = JSON.parse(
+  fs.readFileSync("./ignition/deployments/chain-11155111/artifacts/UniswapV2RouterModule#UniswapV2Router01.json")
+);
+
+const url = "https://eth-sepolia.g.alchemy.com/v2/uwae8IxsUFGbRFh8fagTMrGz1w5iuvpc";
+const network = 11155111;
+
+const provider = new JsonRpcProvider(url, network, {
+  staticNetwork: network,
+});
+
+const contractAddress = "0xC9a3017839de585956Bd699cFeBDA38dA9d5C335";
+const contract = new ethers.Contract(contractAddress, contractABI.abi, provider);
+
+const getWeth = async () => {
+  const weth = await contract.WETH();
+  console.log(weth);
+};
+
+getWeth();
