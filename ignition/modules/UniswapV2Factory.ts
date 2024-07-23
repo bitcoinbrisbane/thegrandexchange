0dc52f45fa1c226e6c000b770046b7c681d7ad8a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-07-24 09:46:12 +1000

    chore: Update deployment addresses and remove MockWethModule

--- ignition/modules/UniswapV2Factory.ts
+++ ignition/modules/UniswapV2Factory.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const UniswapV2FactoryModule = buildModule("UniswapV2FactoryModule", (m) => {
-  const fee_setter = "0x7988123D1F90ccF9675f9D154870Af0f9274DF91";
+  const fee_setter = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // <-- hh account 1  "0x7988123D1F90ccF9675f9D154870Af0f9274DF91";
   const factory = m.contract("UniswapV2Factory", [fee_setter]);
 
   return { factory };
